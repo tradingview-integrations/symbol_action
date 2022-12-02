60fdf7e79dad149037d2da2372aa4057463d6730
Dmitriy Kulagin <86959543+Dmitriy-Kulagin@users.noreply.github.com>
2022-12-02 19:18:10 +0300

    Create map.py
    code idea

---
+++ map.py
@@ -0,0 +1,20 @@
+#!python3
+
+import json
+
+def load_json(fime_name):
+  with open(file_name) as f:
+    res = json.load(f)
+  return res
+
+def main():
+  c_map = {c["cmc-id"]: c["id"] for c in load_json("currency.json_path")
+  group = "param 1"
+  symbol_info = load_json(group)
+  symbol_info["currency"] = [c_map[i] for i in symbol_info["currency-cmc-id"]]
+  symbol_info["base-currency"] = [c_map[i] for i in symbol_info["base-currency-cmc-id"]]
+  del symbol_info["currency-cmc-id"]
+  del symbol_info["base-currency-cmc-id"]
+  with open(group, "w") as f:
+     json.dump(f, symbol_info)
+     
