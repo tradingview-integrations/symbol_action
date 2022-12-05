383df5c60b8a15481e16476bc4571a275c02a067
Dmitriy Kulagin <dkulagin@tradingview.com>
2022-12-05 18:51:06 +0300

    Handle errors

--- map.py
+++ map.py
@@ -1,21 +1,36 @@
 #!python3
+""" map currencies by cmc-id """
 
 import json
 import sys
 
-def load_json(fime_name):
-  with open(file_name) as f:
-    res = json.load(f)
-  return res
+
+def load_json(file_name):
+    """ helper function """
+    with open(file_name) as file:
+        res = json.load(file)
+    return res
+
+
+def curr_map(source, c_map):
+    """ map currencies """
+    return [c_map.get(i, F"ERROR mapping for cmc-id: {i}") for i in source]
+    # for i in source:
+    #     curr_id = c_map.get(i, None)
+    #     # if curr_id is None:
+    #     #     # what to do here ?
+    #     res.append(curr_id)
+    # return res
+
 
 def main():
-  c_map = {c["cmc-id"]: c["id"] for c in load_json("currency.json")
-  group = sys.argv[1]
-  symbol_info = load_json(group)
-  symbol_info["currency"] = [c_map[i] for i in symbol_info["currency-cmc-id"]]
-  symbol_info["base-currency"] = [c_map[i] for i in symbol_info["base-currency-cmc-id"]]
-  del symbol_info["currency-cmc-id"]
-  del symbol_info["base-currency-cmc-id"]
-  with open(group, "w") as f:
-     json.dump(f, symbol_info)
-     
+    """ main routine """
+    c_map = {c["cmc-id"]: c["id"] for c in load_json("currency.json")}
+    group = sys.argv[1]
+    symbol_info = load_json(group)
+    symbol_info["currency"] = curr_map(symbol_info["currency-cmc-id"], c_map)
+    symbol_info["base-currency"] = curr_map(symbol_info["base-currency-cmc-id"], c_map)
+    del symbol_info["currency-cmc-id"]
+    del symbol_info["base-currency-cmc-id"]
+    with open(group, "w") as file:
+        json.dump(file, symbol_info)
